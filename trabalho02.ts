2995d98dee023805aa256f19ba6f383c1f860c12
EDDY_777_BLEH <lima.santos.maria2112@escola.pr.gov.br>
2026-09-10 14:39:38 -0300

    atividade.01

---
+++ trabalho02.ts
@@ -0,0 +1,26 @@
+//atividade 1 
+
+//atividade 2
+
+class Produto{
+
+    nome: string;
+    preco: number;
+    estoque: number;
+
+    constructor (nome: string, preco: number, estoque: number) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+}
+const Pepino = new Produto("Pepino", 67, 9)
+const oleodebebe = new Produto("oleo de bebe", 69, 7)
+
+console.log(Pepino.nome, "\n", Pepino.preco, "\n", Pepino.estoque, "\n", oleodebebe.nome, "\n", oleodebebe.preco, "\n", oleodebebe.estoque)
+
+//atividade 3
+
+//atividade 4
+
+//atividade 5
